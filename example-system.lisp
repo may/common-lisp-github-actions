efdfc2bf3420b0f5420ffd4dd76936a46b93bb82
may <may@users.noreply.github.com>
2024-01-21 15:20:29 -0600

    Compile working; moving to release now.

--- example-system.lisp
+++ example-system.lisp
@@ -1,5 +1,7 @@
 ;;;; example-system.lisp
 
 (in-package #:example-system)
-(print "Hello GitHub Actions!")
-(print "I hope this is useful.")
+(defun hello ()
+  (print "Hello GitHub Actions!")
+  (print "I hope this is useful."))
+(hello)
