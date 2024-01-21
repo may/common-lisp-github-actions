ffbe7a68ab57a7f81a63513ca7eef5316a9967e1
may <may@users.noreply.github.com>
2024-01-21 16:08:26 -0600

    Combining compile and release, conditionally.

--- configure-and-compile-and-release.lisp
+++ configure-and-compile-and-release.lisp
@@ -1,4 +1,6 @@
-;;;; In GitHub actions, setup the Common Lisp envionment in a platform-independent way.
+;;;; In GitHub actions, setup the Common Lisp envionment in a platform-independent
+;;;; way. After that, compile the defined program, and optionally create executables
+;;;; for release.
 ;;; sbcl --non-interactive
 
 ;; Copyright 2024 Nicholas E. May
@@ -14,6 +16,7 @@
 ;; See the License for the specific language governing permissions and
 ;; limitations under the License.
 
+;;; Prep envionment.
 ;; Download & install Quicklisp in the script rather than the GitHub Actions
 ;; because it's easier; the downloaded file is already in our working directory.
 (require :uiop)
@@ -21,8 +24,23 @@
 (load "quicklisp.lisp")
 (quicklisp-quickstart:install)
 
+;;; Compile
 ;; Tell ASDF about the project we're trying to compile.
 ;; ASDF needs directories to have trailing paths.
 (push (concatenate 'string (uiop:getenv "GITHUB_WORKSPACE") "/") asdf:*central-registry*)
 (print asdf:*central-registry*)
 (asdf:compile-system :example-system)
+
+;;; Optionally create release executables.
+;;; We already have the envionment setup, so just keep going.
+(if (uiop:getenv "cl-release")
+    (progn
+      (setq uiop:*image-entry-point* #'main)
+      (uiop:dump-image
+       (if (uiop:os-windows-p)
+           "hello.exe"
+           "hello")
+       :executable t))
+    (print "Not creating executables; configure this in compile-and-release.yaml, line ~40: by setting cl-release to true"))
+
+;; TODO adjust line number as needed.
