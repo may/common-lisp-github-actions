f7df9ca16089e5e84ecdcdfd077aad073722569a
may <may@users.noreply.github.com>
2024-01-21 15:04:00 -0600

    Fix ASDF issue. Add license to main files. Prep for matrix.

--- configure-and-compile.lisp
+++ configure-and-compile.lisp
@@ -1,10 +1,28 @@
+;;;; In GitHub actions, setup the Common Lisp envionment in a platform-independent way.
 ;;; sbcl --non-interactive
+
+;; Copyright 2024 Nicholas E. May
+;; Licensed under the Apache License, Version 2.0 (the "License");
+;; you may not use this file except in compliance with the License.
+;; You may obtain a copy of the License at
+
+;;     http://www.apache.org/licenses/LICENSE-2.0
+
+;; Unless required by applicable law or agreed to in writing, software
+;; distributed under the License is distributed on an "AS IS" BASIS,
+;; WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+;; See the License for the specific language governing permissions and
+;; limitations under the License.
+
+;; Download & install Quicklisp in the script rather than the GitHub Actions
+;; because it's easier; the downloaded file is already in our working directory.
 (require :uiop)
 (uiop:run-program (list "curl" "-O" "https://beta.quicklisp.org/quicklisp.lisp"))
 (load "quicklisp.lisp")
 (quicklisp-quickstart:install)
-; --eval '(ql-util:without-prompting (ql:add-to-init-file))' --quit
-;(push #p"${{ github.workspace }}" asdf:*central-registry*)
-(push (uiop:getenv "GITHUB_WORKSPACE") asdf:*central-registry*)
+
+;; Tell ASDF about the project we're trying to compile.
+;; ASDF needs directories to have trailing paths.
+(push (concatenate 'string (uiop:getenv "GITHUB_WORKSPACE") "/") asdf:*central-registry*)
 (print asdf:*central-registry*)
 (asdf:compile-system :example-system)
